2ca16b0583cb1e471cf300ce75b40a2947f0e8c2
Somya Jha <sumicode.exe@gmail.com>
2023-06-24 20:34:20 -0700

    First comm

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World \n");
+    printf("This is my third code \n");
+    return 0;
+}
+
+// '\n' gives a new line
+// int main(){} ----> it is the common 
+// template to wrtie code in c language
+// include stdio, strings, etc., based on purpoe of code
