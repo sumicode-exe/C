ff4ce875bd3851e362b976d9b727e8f424eaf8f9
Somya Jha <sumicode.exe@gmail.com>
2023-06-26 23:44:04 +0530

    natnum

---
+++ 3/natural-num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    printf("This programme identifies if the number entered is a natural number or not \n");
+    printf("Enter a number: \n");
+
+    int n;
+    scanf("%d", &n);
+
+    if (n>1){
+        printf("Natural Number");
+    } else {
+        printf("Natural Number");
+    }
+
+    return 0;
+
+}
