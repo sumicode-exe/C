a856f0457a92b4f68a6ab708de4dbe667e0c5fee
Somya Jha <sumicode.exe@gmail.com>
2023-06-26 22:41:47 +0530

    ternary compl

--- 3/ternary-operator.c
+++ 3/ternary-operator.c
@@ -1,2 +1,18 @@
 #include<stdio.h>
 
+int main(){
+
+    int age;
+    printf("Enter age : ");
+    scanf("%d", &age);
+
+    age > 18 ? printf("adult \n") : printf("not an adult \n");
+
+    int number = 7;
+    int lucky_number = 7;
+
+    number == lucky_number ? printf("you are lucky \n") : printf("you are not lucky \n");
+
+    return 0;
+        
+}
