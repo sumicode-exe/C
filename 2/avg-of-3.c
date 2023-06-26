615d79317e3600dd7454dcf2e5d6322005b0d3a8
Somya Jha <sumicode.exe@gmail.com>
2023-06-26 21:26:14 +0530

    q1

---
+++ 2/avg-of-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c, avg;
+
+    printf("Enter the first number \n");
+    scanf("%d", &a);
+
+    printf("Enter the second number \n");
+    scanf("%d", &b);
+
+    printf("Enter the third number \n");
+    scanf("%d", &c);
+
+    avg = (a+b+c)/3;
+    printf("Average is : %d", avg);
+
+    return 0;
+}
