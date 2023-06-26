58900765348c099ca62021a3cc6e233f4b478841
Somya Jha <sumicode.exe@gmail.com>
2023-06-26 08:59:49 +0530

    ques

---
+++ Ch1 - Vars, In/Out/Practice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+
+    printf("enter a number \n");
+    scanf("%d", &a);
+
+    printf("enter anothernumber \n");
+    scanf("%d", &b);
+
+    printf("Sum of a & b is : %d \n", a+b);
+
+    return 0;
+}
---
+++ Ch1 - Vars, In/Out/vars.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//This program shoes Variables and Data types
+/*This 
+  Is
+  A
+  Multi
+  Line
+  Comment*/
+
+
+int main(){
+
+    int number;
+    int age;
+    int price;
+
+    int age = 22;
+    float pi = 3.24;
+    char percentage = '%';
+
+    printf("age id %d", age);
+    printf("age is %f", pi);
+    printf("age is %c", percentage);
+
+    return 0;
+}
