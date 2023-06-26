946487e772bb5e76e557aaf5a5f73561c4e7cf17
Somya Jha <sumicode.exe@gmail.com>
2023-06-26 23:25:11 +0530

    dow

---
+++ 3/switch-case.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    char day;
+
+    printf("this programme prints which number (from 1 to 7) corresponds to which day of the week \n");
+    printf("Enter a number from 1 to 7: ");
+    scanf("%d", &day);
+    
+    switch (day) {
+        case 1 : printf("monday \n");
+        break;
+        case 2 : printf("tuesday \n");
+        break;
+        case 3 : printf("wednesday \n");
+        break;
+        case 4 : printf("thursay \n");
+        break;
+        case 5 : printf("friday \n");
+        break;
+        case 6 : printf("saturday \n");
+        break;
+        case 7 : printf("sunday \n");
+        break;
+    }
+
+    return 0;
+}
