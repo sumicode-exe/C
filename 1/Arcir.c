b0da3e7bdb9e3b449d391b73fd4520acfa025005
Somya Jha <sumicode.exe@gmail.com>
2023-06-26 19:34:16 +0530

    final

--- 1/Arcir.c
+++ 1/Arcir.c
@@ -5,7 +5,7 @@ int main(){
     float radius;
 
     printf("Enter the legth of radius \n");
-    scanf("%f", radius);
+    scanf("%f", &radius);
     printf("Area of circle is %f", 3.14*radius*radius);
 
     return 0;
