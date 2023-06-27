9e149b65fa3ea0177a91b0ae7704d399c42793fb
Somya Jha <sumicode.exe@gmail.com>
2023-06-27 21:37:14 +0530

    done

---
+++ functions-and-recursions/marksheet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int calcPercentage (int science, int math, int sanskrit);
+
+int main(){
+    int sc = 98;
+    int math = 95;
+    int sanskrit = 99;
+
+    printf("percentage is : %d", calcPercentage(sc, math, sanskrit));
+    return 0;
+}
+
+int calcPercentage(int science, int math, int sanskrit){
+    return((science + math + sanskrit)/3) * 100;
+}
