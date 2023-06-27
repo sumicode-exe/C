caec871dd55ffa569dd2b2750e0f1182a75fc04b
Somya Jha <sumicode.exe@gmail.com>
2023-06-27 10:05:39 +0530

    sun-of-n

---
+++ loop-control-statements/sum-of-num-bw.c
@@ -0,0 +1,25 @@
+//Programme to find the sum of all the 
+//numbers between the two numbers entered
+//by the user
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int sum = 0;
+    int a, b;
+
+    printf("This programme will find the sum of numbers bw any two numbers \n");
+    printf("To start, enter the first number: \n");
+    scanf("%d", &a);
+    printf("Enter the second number: \n");
+    scanf("%d", &b);
+
+    for(int i = a; i<=b; i++){
+        sum += i;
+    }
+
+    printf("sum id %d", sum);
+
+    return 0;
+}
