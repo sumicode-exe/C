14c7e5654546a98b7af1581e0907d380512c7dea
Somya Jha <sumicode.exe@gmail.com>
2023-06-27 09:57:53 +0530

    revtab

---
+++ loop-control-statements/reverse-of-table.c
@@ -0,0 +1,19 @@
+//This programme prints the reverse
+//of the table for a number n
+//as entered by the user
+//(with iterator and i--)
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int n;
+    printf("enter a number : ");
+    scanf("%d", &n);
+
+    for(int i = 10; i>=1; i--){
+        printf("%d\n", n*i);
+    }
+
+    return 0;
+}
