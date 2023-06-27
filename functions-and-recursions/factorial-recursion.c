641b83b0214de9a7a0f288edf05a795c58d11eb0
Somya Jha <sumicode.exe@gmail.com>
2023-06-27 17:52:51 +0530

    jai mata di

---
+++ functions-and-recursions/factorial-recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int factorial(int n);
+int n, factn;
+
+int main(){
+    printf("enter a number n: \n");
+    scanf("%d", &n);
+    printf("factorial is: %d", factorial(n));
+    return 0;
+}
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    int factnm1 = factorial(n-1);
+    int factn = factnm1 *n;
+    return factn;
+}
---
+++ functions-and-recursions/greeting.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+void namaste();
+void bonjour();
+
+int main(){
+    char I, F, nation;
+    printf("Enter 'I' if you are Indian or 'F' if French \n");
+    scanf("%c", &nation);
+
+    (nation == 'I') ? namaste() : bonjour ();
+}
+
+void namaste(){
+    printf("Namaste!");
+}
+
+void bonjour(){
+    printf("Bonjour!");
+}
