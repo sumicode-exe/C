423b75729bd36c5dcb55f7d2edf1b0d5d6b0a382
Somya Jha <sumicode.exe@gmail.com>
2023-06-27 17:08:27 +0530

    recursions

--- functions-and-recursions/hello-world.c
+++ functions-and-recursions/hello-world.c
@@ -2,3 +2,18 @@
 #include <math.h>
 
 //function declaration/prototype
+void printHello();
+
+int main(){
+    //function call
+    for (int i = 1; i <= 5; i++){
+        printHello();
+    }
+    
+    return 0;
+}
+
+//function defined
+void printHello(){
+    printf("Hello World! \n");
+}
