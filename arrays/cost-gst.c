812468474de7d4016dae8bdf31b78043252d37fd
Somya Jha <sumicode.exe@gmail.com>
2023-06-28 22:09:26 +0530

    stg

---
+++ arrays/cost-gst.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int price[3];
+
+    printf("Cost of objects : ");
+    scanf("%f ", &price[0]);
+    scanf("%f", &price[1]);
+    scanf("%f", &price[2]);
+
+    printf("total price of 1 is: %f \n", price[0] + (0.18 * price[0]));
+    printf("total price of 2 is: %f \n", price[1] + (0.18 * price[1]));
+    printf("total price of 3 is: %f \n", price[2] + (0.18 * price[2]));
+
+    return 0;
+}
