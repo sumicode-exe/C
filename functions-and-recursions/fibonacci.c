5057652f125bb92e201e7b70afd2ee8dd8d54f0f
Somya Jha <sumicode.exe@gmail.com>
2023-06-28 11:17:07 +0530

    fibb

---
+++ functions-and-recursions/fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int fib(int n);
+
+int main(){
+    int n;
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+    fib(n);
+    return 0;
+}
+
+int fib(int n){
+
+    if (n == 0) {
+        return 0;
+    }
+    if(n == 1) {
+        return 1;
+    }
+
+    int fibNm1 = fib(n-1);
+    int fibNm2 = fib(n-2);
+    int fibN = fibNm1 + fibNm2;
+    printf("fib of %d is : &d \n", n, fibN);
+    return fibN;
+}
