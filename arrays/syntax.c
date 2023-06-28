8fa7f9d87d9e5488f849075b14418f797250e572
Somya Jha <sumicode.exe@gmail.com>
2023-06-28 21:51:23 +0530

    syntax - arrays

---
+++ arrays/syntax.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks[3];
+    printf("physics : ");
+    scanf("%d", &marks[0]);
+
+    printf("chemistr : ");
+    scanf("%d", &marks[1]);
+
+    printf("maths : ");
+    scanf("%d", &marks[2]);
+
+    printf("physics = %d, ", marks[0]);
+    printf("chemistry = %d, ", marks[1]);
+    printf("maths = %d, ", marks[2]);
+}
