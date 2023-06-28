aad3f1605a2d38c6664380d7b260d554cc182ecc
Somya Jha <sumicode.exe@gmail.com>
2023-06-28 17:29:01 +0530

    modified:   pointers/a.exe modified:   pointers/syntax.c

--- pointers/syntax.c
+++ pointers/syntax.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
     int age = 22;
     int *ptr = &age;
     int _age = *ptr;
     printf("%d \n", _age);
 
-    //address
+    // address
     printf("%p \n", &age);
     printf("%p \n", ptr);
     printf("%p \n", &ptr);
 
-    //data
+    // data
     printf("%d \n", age);
     printf("%d \n", *ptr);
-    printf("%d \n", * (&age));
+    printf("%d \n", *(&age));
+
+    // OUTPUT?
+    int *a, x;
+    ptr = &x;
+    *ptr = 0;
+
+    printf(" x = %d \n", x);
+    printf(" *ptr = %d \n", *ptr);
+
+    *ptr += 5;
+    printf(" x = %d \n", x);
+    printf(" *ptr = %d \n", *ptr);
+
+    (*ptr)++;
+    printf(" x = %d \n", x);
+    printf(" *ptr = %d \n", *ptr);
 
     return 0;
 }
