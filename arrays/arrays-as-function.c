f57e6c9bf75cc975e19e122eef8cd02808e69c0d
Somya Jha <sumicode.exe@gmail.com>
2023-06-29 16:45:11 +0530

    arrays as function

---
+++ arrays/arrays-as-function.c
@@ -0,0 +1,9 @@
+// ARRAYS AF FUNCTION ARGUMENTS AND SYNTAX
+
+// FUNCTION DECLARATION
+//  void printNumbers(int arr[], int n);
+// or
+//  void printNumbers(int *arr, int n);
+
+// FUNCTION CALL
+//  printNumbers(arr, n);
