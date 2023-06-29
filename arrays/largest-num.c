6565a73026c786264a62612c58227d74b611febe
Somya Jha <sumicode.exe@gmail.com>
2023-06-29 20:40:46 +0530

    revision

--- arrays/largest-num.c
+++ arrays/largest-num.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
+
 int main()
 {
     int size;
+
     printf("Enter the size of the array: ");
     scanf("%d", &size);
+
     int arr[size];
     printf("Enter the elements of the array:\n");
+
     for (int i = 0; i < size; i++)
     {
         scanf("%d", &arr[i]);
     }
-    int x;
-    printf("Enter the number to search for: ");
-    scanf("%d", &x);
-    int count = 0;
-    for (int i = 0; i < size; i++)
+
+    int largest = arr[0];
+    
+    for (int i = 1; i < size; i++)
     {
-        if (arr[i] == x)
+        if (arr[i] > largest)
         {
-            count++;
+            largest = arr[i];
         }
     }
-    printf("The number %d occurs %d times in the array.\n", x, count);
+    printf("The largest number in the array is: %d\n", largest);
     return 0;
 }
