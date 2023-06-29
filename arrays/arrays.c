625e83ed1f0e9abf6e580c8acf7c94fb372b440a
Somya Jha <sumicode.exe@gmail.com>
2023-06-29 14:23:00 +0530

    arrays

---
+++ arrays/arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int aadhaar[5];
+
+    // input
+    int *ptr = &aadhaar[0];
+    for (int i = 0; i<5; i++)
+    {
+        printf("%d index: ", i);
+        scanf("%d", (ptr + i));
+    }
+
+    // output
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d index = %d \n", i, *(ptr + i));
+    }
+
+    return 0;
+}
