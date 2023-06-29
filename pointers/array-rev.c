fc9f59f4a11b3d918f4adc2fafdc43600b306f48
Somya Jha <sumicode.exe@gmail.com>
2023-06-29 20:23:08 +0530

    whew owo

--- pointers/array-rev.c
+++ pointers/array-rev.c
@@ -5,8 +5,10 @@ void printArrayReverse(int arr[], int size);
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
+
     int size = sizeof(arr) / sizeof(arr[0]);
     printf("Array in reverse order: ");
+    
     printArrayReverse(arr, size);
     return 0;
 }
