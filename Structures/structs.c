aa60b5ac57d6e491641049c0fb670863859a7ecd
Somya Jha <sumicode.exe@gmail.com>
2023-07-03 22:24:55 +0530

    student 1 struct details

--- Structures/structs.c
+++ Structures/structs.c
@@ -2,13 +2,15 @@
 #include <math.h>
 #include <string.h>
 
-struct student {
+struct student
+{
     char name[100];
     int roll;
-    float cgpa; 
+    float cgpa;
 };
 
-typedef struct ComputerEngineeringStudent{
+typedef struct ComputerEngineeringStudent
+{
     int roll;
     float cgpa;
     char name[100];
@@ -16,3 +18,12 @@ typedef struct ComputerEngineeringStudent{
 
 void printInfor(struct student s1);
 
+int main()
+{
+    struct student s1;
+    // s1.name = "Somya";
+    // not a modifiable variable
+    strcpy(s1.name, "Somya");
+    s1.roll = 64;
+    s1.cgpa = 9.2;
+}
