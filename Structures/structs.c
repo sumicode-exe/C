ac187e75ce885ea2f09c74082a3c2bab5967648f
Somya Jha <sumicode.exe@gmail.com>
2023-07-03 22:29:44 +0530

    struct students

--- Structures/structs.c
+++ Structures/structs.c
@@ -26,4 +26,19 @@ int main()
     strcpy(s1.name, "Somya");
     s1.roll = 64;
     s1.cgpa = 9.2;
+
+    printf("Sudent info: \n");
+    printf("Name = %s \n", s1.name);
+    printf("Roll No. = %d\n", s1.roll);
+    printf("cgpa = %f\n", s1.cgpa);
+
+    //array of structures
+    struct student IT[60];
+    struct student COE[60];
+    struct student ECE[60];
+
+    //declaration
+    struct student s2 = {"Rajat, 1552, 8.6"};
+    struct student s3 = {0};
 }
+
