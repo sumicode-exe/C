57c498d277facd1847589a33a3216ef599eb30c4
Somya Jha <sumicode.exe@gmail.com>
2023-07-05 15:08:29 +0530

    gets and puts

--- strings/inputs.c
+++ strings/inputs.c
@@ -2,27 +2,32 @@
 #include <math.h>
 #include <string.h>
 
-int main(){
+int main()
+{
+    // declaration
+    char name[] = "Somya Jha";
+    char course[] = {'m', 'a', 't', 'h', 's', ' ', 'c', 'o', 'm', 'p', '\0'};
+    char firstName[] = "Somya";
+    char lastName[] = "Jha";
 
-    //input a string
-    char firstName[40];
-    printf("Enter first name : ");
-    scanf("%s", firstName);
-    printf("Your first name is %s\n", firstName);
+    // input a string
+    // char firstName[40];
+    // printf("Enter first name : ");
+    // scanf("%s", firstName);
+    // printf("Your first name is %s\n", firstName);
 
-    char fullName[40];
-    printf("Enter full name : ");
-    scanf("%s", fullName);
-    printf("Your full name is %s\n", fullName);
+    // char fullName[40];
+    // printf("Enter full name : ");
+    // scanf("%s", fullName);
+    // printf("Your full name is %s\n", fullName);
 
-    //THIS IS THE PROBLEM WITH scanf() AND printf()
-    //IT ONLY CONSIDERS CHARACTERS UPTO SPACE
-    //THAT IS WHY WE USE fgets() AND puts()
+    // THIS IS THE PROBLEM WITH scanf() AND printf()
+    // IT ONLY CONSIDERS CHARACTERS UPTO SPACE
+    // THAT IS WHY WE USE fgets() AND puts()
 
     // gets() & puts()
     char fullName[40];
     printf("Enter full name : ");
     fgets(fullName, 40, stdin);
     puts(fullName);
-
 }
