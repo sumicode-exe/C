14a6d8b988921f9d417e62ad47a935a62344bb40
Somya Jha <sumicode.exe@gmail.com>
2023-07-05 15:44:26 +0530

    preence

---
+++ strings/check-presence.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+void checkChar(char str[], char ch);
+int main()
+{
+    char str[] = "MathematicsAndComputerScienceEngineering";
+    char ch = 'x';
+    checkChar(str, ch);
+}
+
+void checkChar(char str[], char ch)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ch)
+        {
+            printf("character is present!");
+            return;
+        }
+    }
+    printf("character is NOT present :(");
+}
