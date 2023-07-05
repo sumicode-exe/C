7d98101305c1aca2c3f4455aa13d4c50de0facd6
Somya Jha <sumicode.exe@gmail.com>
2023-07-05 15:37:17 +0530

    natnum

---
+++ ternaries/natural-num.c
@@ -0,0 +1,21 @@
+//Programme to check if the
+//Number entered by the user is 
+//A natural number ot not
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    printf("This programme identifies if the number entered is a natural number or not \n");
+    printf("Enter a number: \n");
+
+    int n;
+    scanf("%d", &n);
+
+    n>1 ? printf("Natural Number") :
+    printf("Natural Number");
+    
+    return 0;
+
+}
