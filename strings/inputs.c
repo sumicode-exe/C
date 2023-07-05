d4058b5b94cf56e8fcae072430b4c20921cacd6c
Somya Jha <sumicode.exe@gmail.com>
2023-07-05 14:53:09 +0530

    inoutttt

---
+++ strings/inputs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main(){
+
+    //input a string
+    char firstName[40];
+    printf("Enter first name : ");
+    scanf("%s", firstName);
+    printf("Your first name is %s\n", firstName);
+
+    char fullName[40];
+    printf("Enter full name : ");
+    scanf("%s", fullName);
+    printf("Your full name is %s\n", fullName);
+
+    //THIS IS THE PROBLEM WITH scanf() AND printf()
+    //IT ONLY CONSIDERS CHARACTERS UPTO SPACE
+    //THAT IS WHY WE USE fgets() AND puts()
+}
