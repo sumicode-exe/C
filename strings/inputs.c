c7df112ca8cdcf0e47be0cbeecfeb6dc6378bd34
Somya Jha <sumicode.exe@gmail.com>
2023-07-05 15:02:44 +0530

    fgets

--- strings/inputs.c
+++ strings/inputs.c
@@ -18,4 +18,11 @@ int main(){
     //THIS IS THE PROBLEM WITH scanf() AND printf()
     //IT ONLY CONSIDERS CHARACTERS UPTO SPACE
     //THAT IS WHY WE USE fgets() AND puts()
+
+    // gets() & puts()
+    char fullName[40];
+    printf("Enter full name : ");
+    fgets(fullName, 40, stdin);
+    puts(fullName);
+
 }
