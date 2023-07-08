e7b37e8f974c89408c8b65c0a7f1682ae7c638a4
Somya Jha <sumicode.exe@gmail.com>
2023-07-08 15:08:20 +0530

    countVowels

--- strings/count-vowels.c
+++ strings/count-vowels.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
 
-void countVowels(char str[]);
+// void countVowels(char str[]);
 
-int countVowels (char str[]){
-    int count = 0;
-    for (int i = 0; str[i] != '\0')
-}
+// int countVowels (char str[]){
+//     int count = 0;
+//     for (int i = 0; str[i] != '\0')
+// }
