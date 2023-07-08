7044c06449ed86f0e12ff05a6135903953d0c00c
Somya Jha <sumicode.exe@gmail.com>
2023-07-08 23:03:44 +0530

    checkvowels

--- strings/count-vowels.c
+++ strings/count-vowels.c
@@ -1,10 +1,21 @@
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
+int countVowels(char str[])
+{
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            count++;
+        }
+    }
+}
 
-// void countVowels(char str[]);
-
-// int countVowels (char str[]){
-//     int count = 0;
-//     for (int i = 0; str[i] != '\0')
-// } 
+int main()
+{
+    char str[] = "ApnaCollege";
+    char ch = 'x';
+    checkChar(str, ch);
+}
