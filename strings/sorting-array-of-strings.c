d376114ca112d8243cbdb59548a36ea052397709
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 21:01:51 +0530

    //

--- strings/sorting-array-of-strings.c
+++ strings/sorting-array-of-strings.c
@@ -44,6 +44,6 @@
 //4. The strings consists of lower case english alohabets only. 
 
 //OUTPUT
-The locked code-stub will check the logic of your code. The output consists 
+//The locked code-stub will check the logic of your code. The output consists 
 of the strings sorted according to the four comparsion functions in the order 
 mentioned in the problem statement.
