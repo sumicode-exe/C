c98313fc1a5ca9ba5f55eb3a3c6b6ceec003e8d4
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 20:58:49 +0530

    //

--- strings/sorting-array-of-strings.c
+++ strings/sorting-array-of-strings.c
@@ -3,7 +3,7 @@
 //of comparison strategy can be written. The disadvantage with doing so is having to rewrite the
 //function for every new comparison strategy.
 
-A better implementation would be to write a sorting function that accepts a pointer to the
+//A better implementation would be to write a sorting function that accepts a pointer to the
 function that compares each pair of strings. Doing this will mean only passing a pointer to the
 sorting function with every new comparison strategy.
 
