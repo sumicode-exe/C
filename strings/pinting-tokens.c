34a5466edb122d0038e56b4dbe5f9ce85b1170df
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 20:41:40 +0530

    printnig-tokens

---
+++ strings/pinting-tokens.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char s[1000];
+    scanf("%[^\n]%*c", s);
+    for (int i=0; i<strlen(s); i++)
+    {
+        if (s[i]==' ')
+        {
+            printf("\n");
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    return 0;
+}
