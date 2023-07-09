96e45b1d8322833076fbfac8d2e1c4a5633da675
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 23:27:32 +0530

    salting

--- strings/salting.c
+++ strings/salting.c
@@ -1,17 +1,24 @@
+//pass = "test"
+//salt = "abc"
+//test "testabc123"
+
 #include <stdio.h>
 #include <string.h>
 
 void salting(char password[]);
 
 int main(){
-    
+    char password[100];
+    printf("Enter a password: \n");
+    scanf("%s", &password);
+    salting(password);    
 }
 
 void salting(char password[]){
     char salt[] = "123";
     char newPass[200];
 
-    strcpy(newPass, password);
-    strcat(newPass, salt);
+    strcpy(newPass, password); //newPass = "test";
+    strcat(newPass, salt); // newPass = "test" + "123";
     puts(newPass);
 }
