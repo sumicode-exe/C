6cd768f37d101f30ed569e980e8008767b8c1015
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 20:44:27 +0530

    sorintg

--- strings/sorting-array-of-strings.c
+++ strings/sorting-array-of-strings.c
@@ -1,1 +1,2 @@
 To sort a given array of strings into lexicographically increasing order or into an order in which
+the string with the lowest length appears first, a sorting function with a flag indicating the type
