5853b05926d9d71dbb737e28912fe43f85b1508f
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 16:26:26 +0530

    check char finally works haha

--- strings/check-char.c
+++ strings/check-char.c
@@ -4,10 +4,20 @@
 void checkChar(char str[], char ch);
 int main()
 {
-    char str[] = "Halloajsdhsnasdefrtgyujkiuhr4ew";
-    char ch = 'x';
+
+    char str[50];
+
+    printf("Enter full name : ");
+    fgets(str, 50, stdin);
+    puts(str);
+
+    char ch;
+    printf("Enter the letter to check: ");
+    scanf("%c", &ch);
+
     checkChar(str, ch);
 }
+
 void checkChar(char str[], char ch)
 {
     for (int i = 0; str[i] != '\0'; i++)
@@ -16,7 +26,7 @@ void checkChar(char str[], char ch)
         {
             printf("character is present!");
             return;
-        } 
+        }
     }
     printf("character is NOT present:(");
 }
