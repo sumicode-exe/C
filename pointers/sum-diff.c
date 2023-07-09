d783cf59555d4372e770504f4e837a4c37945848
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 20:39:05 +0530

    commits

--- pointers/sum-diff.c
+++ pointers/sum-diff.c
@@ -1,3 +1,15 @@
+//In order to access the memory address of a variable,
+//prepend it with  sign. For example, &val returns the
+//memory address of .
+
+//This memory address is assigned to a pointer and can
+//be shared among various functions. For example,  
+//will assign the memory address of  to pointer . 
+//To access the content of the memory to which the 
+//pointer points, prepend it with a *. For example,
+//*p will return the value reflected by  and any 
+//modification to it will be reflected at the source ().
+
 #include <stdio.h>
 
 void updates(int *a, int *b){
