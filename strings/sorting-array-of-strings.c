6fbcfc9074faf882a1581d7c3328f1d5e5cdd44d
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 20:44:44 +0530

    disad

--- strings/sorting-array-of-strings.c
+++ strings/sorting-array-of-strings.c
@@ -1,2 +1,3 @@
 To sort a given array of strings into lexicographically increasing order or into an order in which
 the string with the lowest length appears first, a sorting function with a flag indicating the type
+of comparison strategy can be written. The disadvantage with doing so is having to rewrite the
