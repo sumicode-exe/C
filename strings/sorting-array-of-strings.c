f963fa657ae3886b0a938ad40e0f5f5bd970aabe
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 20:57:06 +0530

    //

--- strings/sorting-array-of-strings.c
+++ strings/sorting-array-of-strings.c
@@ -3,9 +3,9 @@ the string with the lowest length appears first, a sorting function with a flag 
 of comparison strategy can be written. The disadvantage with doing so is having to rewrite the
 function for every new comparison strategy.
 
-// A better implementation would be to write a sorting function that accepts a pointer to the
-//function that compares each pair of strings. Doing this will mean only passing a pointer to the
-//sorting function with every new comparison strategy.
+A better implementation would be to write a sorting function that accepts a pointer to the
+function that compares each pair of strings. Doing this will mean only passing a pointer to the
+sorting function with every new comparison strategy.
 
 //Given an array of strings, you need to implement a string_sort function which sorts the
 //strings according to a comparison function, i.e, you need to implement the function :
