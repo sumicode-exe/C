fecea20f9198b935f9cde40b8b7c22200b2559d2
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 20:58:44 +0530

    //

--- strings/sorting-array-of-strings.c
+++ strings/sorting-array-of-strings.c
@@ -1,7 +1,7 @@
 //To sort a given array of strings into lexicographically increasing order or into an order in which
 //the string with the lowest length appears first, a sorting function with a flag indicating the type
 //of comparison strategy can be written. The disadvantage with doing so is having to rewrite the
-function for every new comparison strategy.
+//function for every new comparison strategy.
 
 A better implementation would be to write a sorting function that accepts a pointer to the
 function that compares each pair of strings. Doing this will mean only passing a pointer to the
