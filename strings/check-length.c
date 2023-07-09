f25f6b1f2b6f0745fddd7e3f5c0bb8c6e25f9ce2
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 21:14:43 +0530

    uhi

---
+++ strings/check-length.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void printString(char arr[]);
+int countLength(char arr[]);
+
+int main(){
+    char name[100];
+    fgets(name, 100, stdin);
+    printf("length is : %d", countLength(name));
+    return 0;
+}
+
+
+int countLength(char arr[])
+{
+    int count = 0;
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count - 1;
+}
+
+void printString(char arr[])
+{
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        printf("%c", arr[i]);
+    }
+    printf("\n");
+}
--- strings/slice.c
+++ strings/slice.c
@@ -1,29 +1,29 @@
-// #include <stdio.h>
-// #include <string.h>
-// #include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-// void slice(char str[], int n, int m);
+void slice(char str[], int n, int m);
 
-// int main()
-// {
+int main()
+{
 
-//     char str[50];
-//     int n, m;
+    char str[50];
+    int n, m;
 
-//     printf("Enter full name : ");
-//     fgets(str, 50, stdin);
-//     //puts(str);
-//     slice(str, n, m);
-// }
+    printf("Enter full name : ");
+    fgets(str, 50, stdin);
+    //puts(str);
+    slice(str, n, m);
+}
 
-// void slice(char str[], int n, int m)
-// {
-//     char newStr[100];
-//     int j = 0;
-//     for (int i = n; i <= m; j++, i++)
-//     {
-//         newStr[j] = str[i];
-//     }
-//     newStr[j] = '\0';
-//     puts(newStr);
-// }
+void slice(char str[], int n, int m)
+{
+    char newStr[100];
+    int j = 0;
+    for (int i = n; i <= m; j++, i++)
+    {
+        newStr[j] = str[i];
+    }
+    newStr[j] = '\0';
+    puts(newStr);
+}
