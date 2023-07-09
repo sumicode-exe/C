58774d7499eeb2a81f4f4d35dab2345e3a21c37c
Somya Jha <sumicode.exe@gmail.com>
2023-07-09 23:34:48 +0530

    slice

--- strings/slice.c
+++ strings/slice.c
@@ -1,3 +1,10 @@
+//slice
+//return from index n to m
+//helloWorld - {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+//n = 3 and m = 6;
+//output = loWo
+//this is achieved by making a new string array
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -12,6 +19,13 @@ int main()
 
     printf("Enter full name : ");
     fgets(str, 50, stdin);
+
+    printf("enter from where you want to slice: \n");
+    scanf("%d", &n);
+
+    printf("enter till where you want to slice: \n");
+    scanf("%d", &m);
+
     //puts(str);
     slice(str, n, m);
 }
