1f99d64e9e94aada29555ef2da960279dcb6dc01
Somya Jha <sumicode.exe@gmail.com>
2023-07-10 11:12:49 +0530

    io

---
+++ File-IO/io.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fptr;
+    // Reading a file
+    char ch;
+    fptr = fopen("Test.txt", "r");
+    if (fptr == NULL)
+    {
+        printf("doesnt exist!\n");
+    }
+    else
+    {
+        fscanf(fptr, "%c", &ch);
+        printf("character in file is : %c\n", ch);
+        fscanf(fptr, "%c", &ch);
+        printf("character in file is : %c\n", ch);
+        fclose(fptr);
+    }
+
+    // Writing in a file
+    ch = 'N';
+    fptr = fopen("NewFile.txt", "w");
+    fprintf(fptr, "%cANGO", ch);
+    fclose(fptr);
+
+    // fgets
+    fptr = fopen("NewFile.txt", "r");
+    printf("character in file is : %c\n", fgetc(fptr));
+    printf("character in file is : %c\n", fgetc(fptr));
+    printf("character in file is : %c\n", fgetc(fptr));
+    printf("character in file is : %c\n", fgetc(fptr));
+    printf("character in file is : %c\n", fgetc(fptr));
+    fclose(fptr);
+
+    // fputc
+    fptr = fopen("NewFile.txt", "w");
+    fputc('a', fptr);
+    fpute('p', fptr);
+    fputc('p', fptr);
+    fputc('l', fptr);
+    fputc('e', fptr);
+    fclose(fptr);
+
+    // read the full file (EOF)
+    fptr = fopen("NewFile.txt", "r");
+    ch = fgetc(fptr);
+    while (ch != EOF)
+    {
+        printf("%c", ch);
+        ch = fgetc(fptr);
+    }
+    printf("\n");
+    fclose(fptr);
+}
