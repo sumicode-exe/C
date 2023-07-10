aaba1d1ecb8722efdf710919aa8e96a385e38cb6
Somya Jha <sumicode.exe@gmail.com>
2023-07-10 12:09:02 +0530

    strcs array

--- Structures/array-of-structures.c
+++ Structures/array-of-structures.c
@@ -4,23 +4,16 @@
 
 struct student
 {
-    char name[100];
     int roll;
     float cgpa;
+    char name[100];
 };
-
-// typedef struct ComputerEngineeringStudent
-// {
-//     int roll;
-//     float cgpa;
-//     char name[100];
-// } coe;
 
 int main()
 {
     // declaration
     // array of structures
-    struct student IT[60];
+    struct student IT[100];
     IT[0].roll = 1446;
     IT[0].cgpa = 9.2;
     strcpy(IT[0].name, "Sanjay");
@@ -35,26 +28,23 @@ int main()
     ECE[0].cgpa = 9.5;
     strcpy(ECE[0].name, "Shivank");
 
-    // s2.roll = 96;
-    // printf("Roll No. of s2 = %d \n", s2.roll);
-    // printf("Roll No. of s3 = %d\n", s3.roll);
+    printf("\n");
 
-    // // pointer to structure
-    // struct student *ptr = &s1;
-    // printf("Student.name = %s\n", (*ptr).name);
-    // printf("Student.roll = %d\n", (*ptr).roll);
-    // printf("Student.cgpa = %f\n", (*ptr).cgpa);
+    printf("roll = %d\n", IT[0].roll);
+    printf("cgpa = %f\n", IT[0].cgpa);
+    printf("name = %s\n", IT[0].name);
 
-    // // arrow operator
-    // printf("student->name = %s\n", ptr->name);
-    // printf("student->roll = %d\n", ptr->roll);
-    // printf("student->cgpa = %f\n", ptr->cgpa);
+    printf("\n");
 
-    // // typedef keyword
-    // coe student1;
-    // student1.roll = 1664;
-    // student1.cgpa = 6.7;
-    // strcpy(student1.name, "XYZ");
+    printf("roll = %d\n", COE[0].roll);
+    printf("cgpa = %f\n", COE[0].cgpa);
+    printf("name = %s\n", COE[0].name);
+
+    printf("\n");
+
+    printf("roll = %d\n", ECE[0].roll);
+    printf("cgpa = %f\n", ECE[0].cgpa);
+    printf("name = %s\n", ECE[0].name);
 
     return 0;
 }
