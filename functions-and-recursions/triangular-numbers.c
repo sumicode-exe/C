d5992c7838e45aaca0daaaf3522c63ca8368169d
Somya Jha <sumicode.exe@gmail.com>
2023-07-14 20:02:26 +0530

    triang

---
+++ functions-and-recursions/triangular-numbers.c
@@ -0,0 +1,22 @@
+// FUnction to calculate the nth triangular number
+#include <stdio.h>
+
+void calculateTriangularNumber(int n)
+{
+    int i, triangularNumber = 0;
+
+    for (i = 1; i <= n; i++)
+    {
+        triangularNumber += i;
+
+        printf("Triangular number %i is %i \n", n, triangularNumber);
+    }
+}
+
+int main(void)
+{
+    printf("Enter the number whose triangular you want to calculate: ");
+    int num;
+    scanf("%d", &num);
+    calculateTriangularNumber(num);
+}
