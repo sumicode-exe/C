7d4112dec676374d4cb77045889746159282fa42
Somya Jha <sumicode.exe@gmail.com>
2023-07-23 22:07:33 +0530

    multi

---
+++ Macros/token-pasting-concatenate.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define mergeNUM(a, b) a##b
+// #define mergeCHAR(c, d) #c##d
+#define get(a) #a
+
+int main()
+{
+    printf("%d ", mergeNUM(12, 34));
+    // printf("%s", mergeCHAR(why, i));
+    printf("%s", get(GeeksQuiz));
+}
