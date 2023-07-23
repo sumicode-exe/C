d4ea47e0a5cdd0b1e126f867ace479431cdf9443
Somya Jha <sumicode.exe@gmail.com>
2023-07-23 22:08:07 +0530

    int main() {     int i = 0;     PRINT(i, 3);     return 0; }

--- Macros/multi-line.c
+++ Macros/multi-line.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#define PRINT(i, limit)                                    \
-    while (i < limit) {                                    \
-        printf("GeeksQuiz ");                              \
-        i++;                                               \
+#define PRINT(i, limit)       \
+    while (i < limit)         \
+    {                         \
+        printf("GeeksQuiz "); \
+        i++;                  \
     }
+
+int main()
+{
+    int i = 0;
+    PRINT(i, 3);
+    return 0;
+}
