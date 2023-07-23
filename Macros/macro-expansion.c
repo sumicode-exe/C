e0409fafbc4174a0942483b690b5b437c74817ed
Somya Jha <sumicode.exe@gmail.com>
2023-07-23 22:02:00 +0530

    enter

--- Macros/macro-expansion.c
+++ Macros/macro-expansion.c
@@ -1,12 +1,26 @@
-#include <stdio.h>
+/* #include <stdio.h>
 #define MULTIPLY(a, b) a *b
 
 int main()
 {
-    //THIS MACRO IF EXPANDED AS 
+    //THIS MACRO IF EXPANDED AS
     //2 + 3 * 3 + 5, not as 5 * 8
     printf("%d", MULTIPLY(2 + 3, 3 + 5));
     return 0;
 }
 
-//OUTPUT = 16
+//OUTPUT = 16 */
+
+// ___________________________
+
+// THIS PROBLEM CAN BE SPLVED BY -
+
+#include <stdio.h>
+#define MULTIPLY(a, b) (a) * (b)
+
+int main()
+{
+    // The macro is expanded as (2 + 3) * (3 + 5), as 5*8
+    printf("%d", MULTIPLY(2 + 3, 3 + 5));
+    return 0;
+}
