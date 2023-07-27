117ce7a1117ce11af5e4e7fbfc717349ba0cbc37
Somya Jha <sumicode.exe@gmail.com>
2023-07-27 19:10:00 +0530

    operator or

--- Bitwise-operations/bit.c
+++ Bitwise-operations/bit.c
@@ -49,6 +49,46 @@ G. Explanation
 ----> The maximum possible value of a ^ b is
     also < (k = 4) is 3, so we print 3 third
 
-
-
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  //Write your code here.
+  int maxAND=0, maxOR=0, maxXOR=0, oper;
+  for(int i=1;i<=k;i++)
+  {
+      for(int j=i+1;j<=n;j++){
+          //and
+          oper=i&j;
+          if((oper>maxAND)&&(oper<k)){
+              maxAND=(oper);
+          }
+
+          //or
+          oper=i|j;
+            if((oper>maxOR)&&(oper<k)){
+                maxOR=(oper);
+                }
+          //xor
+          oper=i^j;
+            if((oper>maxXOR)&&(oper<k)){
+                maxXOR=(oper);
+            }
+      }
+  }
+  printf("%d\n%d\n%d",maxAND,maxOR,maxXOR);
+}
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
