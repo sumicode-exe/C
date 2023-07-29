accf372bb437cf931cec0f25c5b1b47c66bad71c
Somya Jha <sumicode.exe@gmail.com>
2023-07-29 17:27:33 +0530

    wor

--- Structures/volume.c
+++ Structures/volume.c
@@ -20,3 +20,23 @@ int is_lower_than_max_height(box b)
     return ((MAX_HEIGHT > b.height) ? 1 : 0);
 }
 
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    box *boxes = malloc(n * sizeof(box));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (is_lower_than_max_height(boxes[i]))
+        {
+            printf("%d\n", get_volume(boxes[i]));
+        }
+    }
+
+    return 0;
+}
