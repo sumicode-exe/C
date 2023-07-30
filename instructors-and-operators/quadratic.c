0ffd2d1b64403efaa872216b6cf3bd19d6653fd6
Somya Jha <sumicode.exe@gmail.com>
2023-07-30 18:22:12 +0530

    //

--- instructors-and-operators/quadratic.c
+++ instructors-and-operators/quadratic.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
     int a, b, c;
     printf("For a quadratic equation, ax^2 + bx + c = 0, ");
     printf("enter the values of a, b and c");
     scanf("%d %d %d", &a, &b, &c);
 
-    int discriminant = sqrt(pow(b, 2) - 4 * a * c);
+    int discriminant = pow(b, 2) - 4 * a * c;
+    if ((discriminant > 0))
+    {
+        double root1 = (-b + sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Root 1: %lf\n", root1);
+        printf("Root 2: %lf\n", root2);
+    }
+    else if (discriminant == 0)
+    {
+
+        double root = -b / (2 * a);
+        printf("Root: %lf\n", root);
+    }
+    else
+    {
+        printf("no real roots");
+    }
 }
-
-
