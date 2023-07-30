5c6d091fb843b483898f1e06b8cfd0fa2be6f895
Somya Jha <sumicode.exe@gmail.com>
2023-07-30 12:46:59 +0530

    //

--- arrays/lcm.c
+++ arrays/lcm.c
@@ -18,18 +18,56 @@ int main()
         scanf("%d", &arr[i]);
     }
 
-    //Calculate the PRODUCT of all elemnts
-    //To calculate LCM, the maximum limit to 
-    //check to would be till the PRODUCT
+    // Calculate the PRODUCT of all elemnts
+    // To calculate LCM, the maximum limit to
+    // check to would be till the PRODUCT
 
     int product = 1;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         product *= arr[i];
+        //return product;
+    }
+    printf("Max can be:%d", product);
+
+    // Calculate the greatest number
+    // The LCM, in any case, will be
+    // MORE than the greatest number
+    //(The table of the greatet number)
+    int greatest = arr[0];
+    for (int i = 0; i <= n; i++)
+    {
+        if (greatest < arr[i])
+        {
+            greatest = arr[i];
+        }
+        // return greatest;
     }
 
-    //Calculate the smallest number
-    //The LCM, in any case, will be 
-    //MORE than the 
+    // For each mutiple of the greatest number,
+    // if other elements in the array divide it
+    // to remainder 0, it is the LCM
+    int lcm = 1;
+    for (int i = greatest; i <= product + 1; i *= greatest)
+    {
+        // int isDivisible = 1; // Flag variable
+        // for (int j = 0; j < n; j++)
+        // {
+        //     if (i % arr[j] != 0)
+        //     {
+        //         isDivisible = 0;
+        //         break;
+        //     }
+        // }
+        // if(isDivisible == 1){
+        //     lcm = i;
+        //     break;
+        // }
+        printf("table: %d \n", i);
+    }
     
 
-}
+
+    //printf("%d", lcm);
+    return 0;
+    }
