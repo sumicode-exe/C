8576497cf00e41b395233084e1b496d63e5acb6f
Somya Jha <sumicode.exe@gmail.com>
2023-08-09 21:06:56 +0530

    comments changes

--- arrays/array-matrix.c
+++ arrays/array-matrix.c
@@ -3,8 +3,10 @@
 
 int main()
 {
-    // 2 x 3
+    // Declare a 2D array of size 2x3 to store marks
     int marks[2][3]; // _ _ _ _ _ _
+
+    // Assign values to the elements of the array
     marks[0][0] = 90;
     marks[0][1] = 89;
     marks[0][2] = 98;
@@ -13,11 +15,14 @@ int main()
     marks[1][1] = 99;
     marks[1][2] = 90;
 
-    printf("%d \t", marks[0][0]);
-    printf("%d \t", marks[0][1]);
-    printf("%d \t \n", marks[0][2]);
+    // Print the elements of the array
+    printf("%d \t", marks[0][0]); // Print the value at row 0, column 0
+    printf("%d \t", marks[0][1]); // Print the value at row 0, column 1
+    printf("%d \t \n", marks[0][2]); // Print the value at row 0, column 2
 
-    printf("%d \t", marks[1][0]);
-    printf("%d \t", marks[1][1]);
-    printf("%d \t", marks[1][2]);
+    printf("%d \t", marks[1][0]); // Print the value at row 1, column 0
+    printf("%d \t", marks[1][1]); // Print the value at row 1, column 1
+    printf("%d \t", marks[1][2]); // Print the value at row 1, column 2
+
+    return 0;
 }
