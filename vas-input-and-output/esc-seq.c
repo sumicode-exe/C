a73bf624be4fb1d405b2ef8f679ef40286f4a07f
Somya Jha <sumicode.exe@gmail.com>
2024-04-09 21:41:32 +0530

    assgnment

--- vas-input-and-output/esc-seq.c
+++ vas-input-and-output/esc-seq.c
@@ -1,10 +1,24 @@
+// #include <stdio.h>
+
+// int main() {
+//     printf("Hello\tWorld!\n");  // \t is used for horizontal tab
+//     printf("This is a new line.\n");  // \n is used for new line
+//     printf("Backslash: \\ \n");  // To print a backslash, you need to use
+//     printf("Double quotes: \"Hello!\" \n");  // To print double quotes, you need to use \"
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int main() {
-    printf("Hello\tWorld!\n");  // \t is used for horizontal tab
-    printf("This is a new line.\n");  // \n is used for new line
-    printf("Backslash: \\ \n");  // To print a backslash, you need to use \\
-    printf("Double quotes: \"Hello!\" \n");  // To print double quotes, you need to use \"
-
-    return 0;
+int main()
+{
+    printf("single quotes - Hello \'World\' \n");
+    printf("double quotes - Hello \"World\" \n");
+    printf("question mark - Hello World\? \n");
+    printf("backslash - Hello \\World\\ \n");
+    printf("newline - Hello \n World \n");
+    printf("horizontal tab - Hello \t World \n");
+    printf("vertical tab - Hello \v World \n");
+    printf("carriage return - Hello \r World \n");
 }
